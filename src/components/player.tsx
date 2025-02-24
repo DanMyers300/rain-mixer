57a5cdc9c6a925447756afcfa0bab6b4f98ac14f
dan <contact@danmyers.net>
2025-02-24 17:34:51 -0600

    added spinning record

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
+import record from '../../assets/record.png';
+
 const Player = () => {
+  const [spin, setSpin] = useState(false);
+
+  const toggleSpin = () => {
+    setSpin(!spin);
+  };
+
   return (
-    <>
-      <div className="m-3 flex h-20 w-20 items-center justify-center rounded-full bg-blue-600">
-        <p className="text-white">Circle</p>
-      </div>
-    </>
+    <div 
+      onClick={toggleSpin}
+      className="
+        m-3
+        flex
+        h-80
+        w-80
+        items-center
+        justify-center
+        rounded-full
+        cursor-pointer
+        overflow-hidden
+        relative
+      "
+    >
+      <img 
+        src={record}
+        alt="record"
+        className={`
+          absolute
+          inset-0
+          h-full
+          w-full
+          object-cover
+          ${spin ? 'animate-[spin_10s_linear_infinite]' : ''}
+        `}
+      />
+    </div>
   );
 };
 
