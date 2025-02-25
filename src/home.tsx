0baf29c03d2838b155207a16b2bfbdfd445ada6c
dan <contact@danmyers.net>
2025-02-25 12:46:25 -0600

    working on github pages

---
+++ src/home.tsx
@@ -0,0 +1,11 @@
+import Record from './components/Player.tsx';
+
+const Home = () => {
+  return (
+    <main className="flex items-center justify-center h-screen w-screen">
+      <Record />
+    </main>
+  );
+};
+
+export default Home;
