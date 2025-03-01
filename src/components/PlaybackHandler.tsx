0656bc46ad5f5fcba448082606e22ffef67f0138
dan <contact@danmyers.net>
2025-02-28 19:43:59 -0600

    Added volume slider

--- src/components/PlaybackHandler.tsx
+++ src/components/PlaybackHandler.tsx
@@ -3,9 +3,10 @@ import { useEffect, useRef } from 'react';
 interface PlaybackHandlerProps {
   playing: boolean;
   track: string;
+  volume: number;
 }
 
-const PlaybackHandler = ({ playing, track }: PlaybackHandlerProps) => {
+const PlaybackHandler = ({ playing, track, volume }: PlaybackHandlerProps) => {
   const audio = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
@@ -13,6 +14,12 @@ const PlaybackHandler = ({ playing, track }: PlaybackHandlerProps) => {
       playing ? audio.current.play() : audio.current.pause();
     }
   }, [playing]);
+
+  useEffect(() => {
+    if (audio.current) {
+      audio.current.volume = volume;
+    }
+  }, [volume]);
 
   return (
     <audio ref={audio} loop>
