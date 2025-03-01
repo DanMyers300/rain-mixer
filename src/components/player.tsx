3365e3df60c72060ed3a3ede525ec5df91a9de12
dan <contact@danmyers.net>
2025-02-28 20:11:55 -0600

    refactor

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,23 +1,17 @@
-import { useState } from 'react';
-
-// Components
-import PlaybackHandler from './playbackHandler.tsx';
-
-// Assets
 import record from '../../assets/record.png';
-import rain from '../../assets/rain.mp3'
+import rain from '../../assets/rain.mp3';
+import Audio from './playbackHandler.tsx';
 
-const Record = () => {
-  const [spin, setSpin] = useState(false);
-  const [volume, setVolume] = useState(0.5);
+interface RecordProps {
+  volume: number;
+  playing: boolean;
+  togglePlaying: () => void;
+}
 
-  const toggleSpin = () => {
-    setSpin(!spin);
-  };
-
+const Record = ({ volume, playing, togglePlaying }: RecordProps) => {
   return (
     <div
-      onClick={toggleSpin}
+      onClick={togglePlaying}
       className="
         m-3
         flex
@@ -40,42 +34,10 @@ const Record = () => {
           h-full
           w-full
           object-cover
-          ${spin ? 'animate-[spin_10s_linear_infinite]' : ''}
+          ${playing ? 'animate-[spin_10s_linear_infinite]' : ''}
         `}
       />
-      
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.1"
-        value={volume}
-        onChange={(e) => setVolume(parseFloat(e.target.value))}
-        onClick={(e) => e.stopPropagation()}
-        className="
-          absolute
-          bottom-4
-          left-1/2
-          transform
-          -translate-x-1/2
-          w-32
-          h-2
-          bg-gray-200
-          rounded-lg
-          appearance-none
-          cursor-pointer
-          hover:opacity-100
-          opacity-80
-          transition-opacity
-          [&::-webkit-slider-thumb]:appearance-none
-          [&::-webkit-slider-thumb]:h-4
-          [&::-webkit-slider-thumb]:w-4
-          [&::-webkit-slider-thumb]:rounded-full
-          [&::-webkit-slider-thumb]:bg-white
-        "
-      />
-
-      <PlaybackHandler playing={spin} track={rain} volume={volume} />
+      <Audio playing={playing} track={rain} volume={volume} />
     </div>
   );
 };
