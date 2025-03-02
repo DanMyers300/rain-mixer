4153aab53b33b07f58dcb0dedc5cc7e66ae44d9a
dan <contact@danmyers.net>
2025-03-02 17:46:17 -0600

    Electron fully working

--- flake.nix
+++ flake.nix
@@ -6,6 +6,47 @@
   outputs = { self, nixpkgs, ... } @ inputs: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
+    # Common libraries function parameterized by pkgs
+    commonLibraries = pkgs: with pkgs; [
+      # Libraries from the original buildInputs
+      libxkbcommon
+      cups
+      xorg.libXtst
+      xorg.libXrandr
+      xorg.libXdamage
+      xorg.libXcomposite
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXext
+      xorg.libX11
+      xorg.libXrender
+      xorg.libXfixes
+      xorg.libxcb
+      pango
+      cairo
+      gdk-pixbuf
+      glib
+      gtk3
+      atk
+      dbus.lib
+      expat
+      fontconfig
+      freetype
+      alsa-lib
+      nss
+      nspr
+      mesa
+
+      # Additional libraries from the nix-ld example
+      stdenv.cc.cc
+      zlib
+      fuse3
+      icu
+      openssl
+      curl
+    ];
+
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
@@ -14,69 +55,17 @@
         typescript
         electron
         tailwindcss
-        mesa
         p7zip
         appimage-run
         autoPatchelfHook
-        
-        libxkbcommon
-        xorg.libXtst
-        xorg.libXrandr
-        xorg.libXdamage
-        xorg.libXcomposite
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXext
-        xorg.libX11
-        xorg.libXrender
-        xorg.libXfixes
-        pango
-        cairo
-        gdk-pixbuf
-        glib
-        gtk3
-        atk
-        dbus.lib
-        expat
-        fontconfig
-        freetype
-        alsa-lib
-        nss
-        nspr
         
         python3
         gcc
         gnumake
         pkg-config
-      ];
+      ] ++ (commonLibraries pkgs);
 
-      LD_LIBRARY_PATH = with pkgs; "${lib.makeLibraryPath [
-        libxkbcommon
-        xorg.libXtst
-        xorg.libXrandr
-        xorg.libXdamage
-        xorg.libXcomposite
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXext
-        xorg.libX11
-        xorg.libXrender
-        xorg.libXfixes
-        pango
-        cairo
-        gdk-pixbuf
-        glib
-        gtk3
-        atk
-        dbus.lib
-        expat
-        fontconfig
-        freetype
-        alsa-lib
-        nss
-        nspr
-        mesa
-      ]}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+      LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (commonLibraries pkgs)}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
 
       shellHook = ''
         export ELECTRON_SKIP_BINARY_DOWNLOAD=1
@@ -89,6 +78,14 @@
       '';
 
       ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin";
+    };
+
+    # NixOS module configuring nix-ld with the same libraries
+    nixosModules.default = { config, pkgs, ... }: {
+      programs.nix-ld = {
+        enable = true;
+        libraries = commonLibraries pkgs;
+      };
     };
   };
 }
