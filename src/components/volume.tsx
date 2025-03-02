cd373375593da17e741ff2f240c5c556afc1809b
dan <contact@danmyers.net>
2025-03-02 13:36:00 -0600

    Added styling to volume slider

--- src/components/volume.tsx
+++ src/components/volume.tsx
@@ -18,16 +18,19 @@ const Volume = ({ volume, setVolume }: VolumeProps) => {
       value={volume}
       onChange={handleChange}
       onClick={(e) => e.stopPropagation()}
-      className="mt-5 w-48 h-2 rounded-full
-        appearance-none bg-gradient-to-r from-transparent
-        via-current to-transparent cursor-pointer
+      className="mt-5 w-48 h-4 rounded-full
+        appearance-none cursor-pointer
         [&::-webkit-slider-container]:bg-transparent
-        [&::-webkit-slider-runnable-track]:rounded-[100px/1px]
         [&::-webkit-slider-thumb]:appearance-none
         [&::-webkit-slider-thumb]:h-4
         [&::-webkit-slider-thumb]:w-4
         [&::-webkit-slider-thumb]:rounded-full
-        [&::-webkit-slider-thumb]:bg-white"
+        [&::-webkit-slider-thumb]:bg-white
+        [&::-webkit-slider-thumb]:relative
+        [&::-webkit-slider-thumb]:z-20"
+      style={{
+        '--volume': `${volume * 100}%`,
+      } as React.CSSProperties}
     />
   );
 };
