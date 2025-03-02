fe32f5f3b0d891f6da3df98ea33b886d55717eaa
dan <contact@danmyers.net>
2025-03-01 20:26:35 -0600

    created player component

--- src/home.tsx
+++ src/home.tsx
@@ -1,22 +1,16 @@
-import { useState } from 'react';
-
-import Record from './components/record.tsx';
-import Volume from './components/volume.tsx';
+import Player from './components/player.tsx';
 
 const Home = () => {
-  const [volume, setVolume] = useState(0.25);
-  const [playing, setPlaying] = useState(false);
-
-  const togglePlaying = () => {
-    setPlaying(!playing);
-  };
-
   return (
-    <main className="flex flex-col w-screen h-screen items-center justify-center" >
-      <div id="Player" className="flex flex-col w-64 items-center">
-        <Record volume={volume} playing={playing} togglePlaying={togglePlaying} />
-        <Volume volume={volume} setVolume={setVolume} />
-      </div>
+    <main className="
+      flex
+      flex-col
+      w-screen
+      h-screen
+      items-center
+      justify-center"
+    >
+      <Player />
     </main>
   );
 };
