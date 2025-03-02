1869efa340f3441d3c82ac97d6fe398c7872c4c8
dan <contact@danmyers.net>
2025-03-02 16:28:28 -0600

    added dockerfile

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
-  inputs={
+  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs={
-    self,
-    nixpkgs,
-    ...
-  } @ inputs:
-      let
-      inherit (self) outputs;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-  in with pkgs; {
-    devShells.${system}.default = mkShell {
+  outputs = { self, nixpkgs, ... } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         bun
         nodejs_23
@@ -21,8 +15,80 @@
         electron
         tailwindcss
         mesa
+        p7zip
+        appimage-run
+        autoPatchelfHook
+        
+        libxkbcommon
+        xorg.libXtst
+        xorg.libXrandr
+        xorg.libXdamage
+        xorg.libXcomposite
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXext
+        xorg.libX11
+        xorg.libXrender
+        xorg.libXfixes
+        pango
+        cairo
+        gdk-pixbuf
+        glib
+        gtk3
+        atk
+        dbus.lib
+        expat
+        fontconfig
+        freetype
+        alsa-lib
+        nss
+        nspr
+        
+        python3
+        gcc
+        gnumake
+        pkg-config
       ];
-      LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+
+      LD_LIBRARY_PATH = with pkgs; "${lib.makeLibraryPath [
+        libxkbcommon
+        xorg.libXtst
+        xorg.libXrandr
+        xorg.libXdamage
+        xorg.libXcomposite
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXext
+        xorg.libX11
+        xorg.libXrender
+        xorg.libXfixes
+        pango
+        cairo
+        gdk-pixbuf
+        glib
+        gtk3
+        atk
+        dbus.lib
+        expat
+        fontconfig
+        freetype
+        alsa-lib
+        nss
+        nspr
+        mesa
+      ]}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+
+      shellHook = ''
+        export ELECTRON_SKIP_BINARY_DOWNLOAD=1
+        export npm_config_build_from_source=true
+        export npm_config_ignore_scripts=true
+        
+        # Patch node_modules after installation
+        autoPatchelf ./node_modules/7zip-bin/linux/x64/
+        autoPatchelf ./node_modules/app-builder-bin/linux/x64/
+      '';
+
+      ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin";
     };
   };
 }
