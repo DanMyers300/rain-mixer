96c79789e270b623568e9ebcbcf036043b09620e
dan <contact@danmyers.net>
2025-03-02 21:40:13 -0600

    removed dockerfile for now

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Start user-level DBUS session
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
-mkdir -p /run/user/$(id -u)
-dbus-daemon --session --fork --address=$DBUS_SESSION_BUS_ADDRESS
-
-# Run the application
-exec "$@"
