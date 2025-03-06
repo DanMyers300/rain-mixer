85b61ec97f6df6fcd3ae18faa54569552e0e65a4
dan <contact@danmyers.net>
2025-03-06 15:28:06 -0600

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -4,53 +4,62 @@
   };
 
   outputs = { self, nixpkgs, ... } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    systems = [ "x86_64-linux" ];  # Add other systems here if needed
+    
+    forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f {
+      inherit system;
+      pkgs = nixpkgs.legacyPackages.${system};
+    });
 
-    buildLibraries = with pkgs; [
-      nspr nss dbus atk at-spi2-atk
-      at-spi2-core cups gtk3 pango
-      cairo libxkbcommon libdrm
-      expat udev alsa-lib libgcc
-      mesa ffmpeg xorg.libX11
-      xorg.libXcomposite xorg.libXdamage
-      xorg.libXext xorg.libXfixes
-      xorg.libXrandr xorg.libxcb
-      libappindicator-gtk3
-      libuuid
-      musl
-    ];
+  in {
+    packages = forEachSystem ({ pkgs, system }: let
+      buildLibraries = with pkgs; [
+        nspr nss dbus atk at-spi2-atk
+        at-spi2-core cups gtk3 pango
+        cairo libxkbcommon libdrm
+        expat udev alsa-lib libgcc
+        mesa ffmpeg xorg.libX11
+        xorg.libXcomposite xorg.libXdamage
+        xorg.libXext xorg.libXfixes
+        xorg.libXrandr xorg.libxcb
+        libappindicator-gtk3
+        libuuid musl
+      ];
+    in {
+      default = pkgs.stdenv.mkDerivation rec {
+        name = "rain-mixer";
+        src = pkgs.fetchzip {
+          url = "https://github.com/DanMyers300/rain-mixer/releases/download/latest/rain-mixer.tar.gz";
+          hash = "sha256-azzuFBmsfrQsRhinBsQrkf+IC0/dh+eYzAVDTo7rb18=";
+        };
 
-    devLibraries = with pkgs; [
-      electron
-      nodejs_23
-      bun
-    ];
+        buildInputs = buildLibraries;
+        nativeBuildInputs = [
+          pkgs.autoPatchelfHook
+          pkgs.patchelf
+        ] ++ buildLibraries;
 
-  in with pkgs; {
-    devShells.${system}.default = mkShell {
-      name = "dev";
-      buildInputs = devLibraries;
-    };
-
-    packages.${system}.default = stdenv.mkDerivation rec {
-      name = "rain-mixer";
-      src = fetchzip {
-        url = "https://github.com/DanMyers300/rain-mixer/releases/download/latest/rain-mixer.tar.gz";
-        hash = "sha256-azzuFBmsfrQsRhinBsQrkf+IC0/dh+eYzAVDTo7rb18=";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp -r linux-unpacked/* $out/bin/
+          chmod +x $out/bin/rain-mixer
+        '';
       };
+    });
 
-      buildInputs = buildLibraries;
-      nativeBuildInputs = [
-        pkgs.autoPatchelfHook
-        pkgs.patchelf
-      ] ++ buildLibraries;
+    devShells = forEachSystem ({ pkgs, system }: {
+      default = pkgs.mkShell {
+        name = "dev";
+        buildInputs = with pkgs; [
+          electron nodejs_23 bun
+        ];
+      };
+    });
 
-      installPhase = ''
-        mkdir -p $out/bin
-        cp -r linux-unpacked/* $out/bin/
-        chmod +x $out/bin/rain-mixer
-      '';
-    };
+    defaultPackage = forEachSystem ({ pkgs, system }: self.packages.${system}.default);
+    defaultApp = forEachSystem ({ pkgs, system }: {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/rain-mixer";
+    });
   };
 }
